f5938e127cf96f3ea87d367dc867c26d7e76e51e
LUCASYUKR <78840755+LUCASYUKR@users.noreply.github.com>
2021-02-18 09:51:53 +0900

    Update DB.py

--- DB.py
+++ DB.py
@@ -1,10 +1,10 @@
 import pymysql
 
 
-db = pymysql.connect(host="localhost",
-                     port=3306,
-                     user="javauser",
-                     passwd="1234",
-                     db="javadb",
+db = pymysql.connect(host="",
+                     port=,
+                     user="",
+                     passwd="",
+                     db="",
                      charset="utf8")
 cursor = db.cursor()
